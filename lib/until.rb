1e64a8a2a2ada93c4d6546b6fce0fc19b66145dc
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-28 19:10:34 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,9 +2,8 @@ def using_until
   levitation_force = 6
   levitation_force =+ 1
   until levitation_force == 10
-  
   puts "Wingardium Leviosa"
-  break
+  break when levitation_force > 10
 end
 end
 
